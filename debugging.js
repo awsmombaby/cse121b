a9bf55ae739c5c1bdd8514c136b2b2b8aed2c7de
awsmombaby <seirann0217@gmail.com>
2024-01-18 21:07:46 +0800

    w01-w02 practice

---
+++ debugging.js
@@ -0,0 +1,40 @@
+const PI = 3.14;
+// let radius = 3;
+let area = 0;
+// area = radius * radius * PI;
+// console.log("Area1:", area) ;
+// radius = 4;
+// area = radius * radius * PI;
+// console.log("Area2:", area);
+function circleArea(radius){
+    const area = radius * radius * PI;
+    return area;
+}
+area = circleArea(3);
+console.log("Area1: ", area);
+
+// radius = 4;
+area = circleArea(4);
+console.log("Area2: ", area)
+
+console.log(typeof 42)  //console.log 輸出到除錯 
+console.log(typeof "abc") //alert 輸出到跳出視窗
+console.log(typeof true)  //document.write 輸出到瀏覽器頁面上
+console.log(typeof undefined)
+console.log(typeof null)
+console.log(typeof { a: 1 })
+console.log(typeof [1, 2, 3])
+console.log(typeof function hello() {})
+
+var adult = true;
+
+if (adult) {
+  var myName = "Bob";
+  var age = 24;
+}
+
+console.log(myName);
+// Bob
+
+console.log(age);
+// Error!
---
+++ w02-test.js
@@ -0,0 +1,12 @@
+let one = 1;
+let two = 2;
+const add = one + two;
+const multi = one * two;
+const subtr = two - one;
+const conca = String(one) + String(two);
+two = 4;
+const increment = one + 3;
+const compare = one == two;
+const lessthen = two < one;
+const notEqual = one != two;
+const lessOrgr = two < 10 & two > 0;
